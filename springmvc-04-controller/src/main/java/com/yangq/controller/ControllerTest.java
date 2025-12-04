07fd2acbe12ed33caa4ea0351bd384edec01148c
hello28256 <hello28256@gmail.com>
2025-12-04 14:46:47 +0800

    SpringMVC

---
+++ springmvc-04-controller/src/main/java/com/yangq/controller/ControllerTest.java
@@ -0,0 +1,28 @@
+package com.yangq.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author yangq
+ * @version 1.0
+ * @description: TODO
+ * @date 2025/12/4 09:53
+ */
+
+// 定义控制器:只要实现了 Controller 接口类，说则这就是一个控制器了
+// 注意点：不要导错包，实现Controller接口，重写方法；
+public class ControllerTest implements Controller {
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        //返回一个模型视图对象
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("msg","ControllerTest");
+        mv.setViewName("test");
+        return mv;
+    }
+}
+
+
